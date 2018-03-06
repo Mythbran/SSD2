0f7671a4c0fe957e8616de76a6eefd77da8a852b
Mythbran <matthewdangelo@hotmail.com>
2018-03-05 19:53:59 -0500

    master

--- index.php
+++ index.php
@@ -41,17 +41,17 @@
           <a class="navbar-brand" href="/">Home</a>
         </div>
         <?php
-          echo "<div id='navbar' class='navbar-collapse collapse'> "
-          echo "<form class='navbar-form navbar-right' role='form'>"
-          echo "  <div class='form-group'>"
-          echo "    <input type='text' placeholder='Username' class='form-control'>"
-          echo "   </div>"
-          echo "  <div class='form-group'>"
-          echo "    <input type='password' placeholder='Password' class='form-control'>"
-          echo "  </div>"
-          echo "  <button type='submit' class='btn btn-success'>Sign in</button>"
-          echo "</form>"
-        echo "</div><!--/.navbar-collapse -->"
+          echo "<div id='navbar' class='navbar-collapse collapse'> 
+          <form class='navbar-form navbar-right' role='form'>
+           <div class='form-group'>
+              <input type='text' placeholder='Username' class='form-control'>
+             </div>
+            <div class='form-group'>
+              <input type='password' placeholder='Password' class='form-control'>
+           </div>
+            <button type='submit' class='btn btn-success'>Sign in</button>
+          </form>
+        </div><!--/.navbar-collapse -->";
 
         ?>
 
